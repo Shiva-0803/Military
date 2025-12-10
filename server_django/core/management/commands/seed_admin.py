facb51e807963f9fb288838a352b1e084b8a41cb
Shiva-0803 <goudashivasai2@gmail.com>
2025-12-10 21:42:34 +0530

    Fix Render: Add seed_admin command

---
+++ server_django/core/management/commands/seed_admin.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from core.models import User
+import os
+
+class Command(BaseCommand):
+    help = 'Creates a superuser/admin if none exists'
+
+    def handle(self, *args, **kwargs):
+        username = os.environ.get('ADMIN_USERNAME', 'admin')
+        password = os.environ.get('ADMIN_PASSWORD', 'admin123')
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Successfully created admin user: {username}'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Admin user {username} already exists'))
